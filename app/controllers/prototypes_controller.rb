2155db1e678be1ae316a12b1a0b575aaac1b142e
Hiro0608 <h.satou1991@gmail.com>
2022-10-19 23:39:14 +0900

    投稿機能実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,10 +1,29 @@
 class PrototypesController < ApplicationController
   def index
+    @prototypes = Prototype.includes(:user)
   end
 
   def new
+    @prototype = Prototype.new
   end
 
   def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :place, :introduction, :image).merge(user_id: current_user.id)
   end
 end
